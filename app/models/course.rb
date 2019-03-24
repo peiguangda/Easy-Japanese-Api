af12bd26a696d2f9621973bc1a30d2b5b8cb3eec
ngango0205 <quynhnga27231096@gmail.com>
2019-03-25 00:27:02 +0700

    fix api seed

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :user_courses, dependent: :destroy
-  has_many :topics
-  has_many :cards
+  has_many :topics, dependent: :destroy
+  has_many :cards, dependent: :destroy
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ApplicationRecord
   belongs_to :course
   has_many :topic_progresses, dependent: :destroy
   belongs_to :user, optional: true
-  has_many :cards
+  has_many :cards, dependent: :destroy
 
   def set_user_create_topic user
     self.user_id = user.id

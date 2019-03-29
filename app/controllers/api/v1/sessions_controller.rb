2c877d30233e4ba6d1ff1b908b9cd70138fe796c
Quang Dai <peiguangda@gmail.com>
2019-03-29 11:36:05 +0000

    add show profile

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,14 +1,14 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
   def create
     user_password = params[:session][:password]
-    user_email = params[:session][:email]
-    user = user_email.present? && User.find_by(email: user_email)
+    user_account = params[:session][:account]
+    user = user_account.present? && User.find_by(account: user_account)
 
-    if user.valid_password? user_password
+    if user && (user.valid_password? user_password)
       sign_in user, store: false
       user.generate_authentication_token!
       user.save
-      render json: user, status: 200, location: [:api, user]
+      render json: {data: user, status: "success"}, status: 200, location: [:api, user]
     else
       render json: {errors: "Invalid email or password"}, status: 422
     end
